0338f090a624f4ab21b132d1eea63fbf4b634364
lu3eSkywalker <surajjhajhria1@gmail.com>
2024-12-20 19:20:17 +0530

    Adding full backend code

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -11,30 +11,44 @@ const baseRpcUrl = process.env.BASE_RPC_URL;
 const ethereumPrivateKey = process.env.ETH_PRIVATE_KEY;
 const basePrivateKey = process.env.BASE_PRIVATE_KEY;
 
-const lockUnlockAddress = "0xEbf653876160b9FeF203e7142A2235C41a6327D0";
+// const lockUnlockAddress = "0xEbf653876160b9FeF203e7142A2235C41a6327D0";
+const lockUnlockAddress = "0xEb5075AE5d8Ff0a22f41aC8F4E9f3D72170b9ce7";
 const baseContractAddress = "0x0D5809846D1cA42Fa361E91399F770CB0a4824ED";
 
-const abi = [
-  "event LockEvent(address sender, uint256 _tokenValue)",
+const abiETH = [
+  "event LockEvent(address sender, uint256 _tokenValue)"
+];
+
+const abiBASE = [
+    "event tokenBurn(address _from, uint256 _value)"
+];
+
+
+const ethABI = [
+    "function unlockTokens(address _to, uint256 _tokenValue)",
+    "function tokenBurnedOnBaseChain(address _to, uint256 _tokenValue)"
 ];
 
 const baseAbi = [
     "function mint(address _to, uint256 _amount)"
-]
+];
 
 const ethereumProvider = new ethers.JsonRpcProvider(ethereumRpcUrl);
 const ethereumWallet = new ethers.Wallet(ethereumPrivateKey, ethereumProvider);
+const ethereumContract = new ethers.Contract(lockUnlockAddress, ethABI, ethereumWallet);
 
 const baseProvider = new ethers.JsonRpcProvider(baseRpcUrl);
 const baseWallet = new ethers.Wallet(basePrivateKey, baseProvider);
 const baseContract = new ethers.Contract(baseContractAddress, baseAbi, baseWallet);
 
-const lockContract = new ethers.Contract(lockUnlockAddress, abi, ethereumProvider);
+const lockContract = new ethers.Contract(lockUnlockAddress, abiETH, ethereumProvider);
+
+const burnTokensContract = new ethers.Contract(baseContractAddress, abiBASE, baseProvider);
 
 
 // Token Mint If Token Lock
 lockContract.on("LockEvent", async (sender, _tokenValue) => {
-    console.log(`Event Log Detected: By the TokenAddress: ${sender}, TokenValue: ${_tokenValue}`);
+    console.log(`Event Log Detected: From the Sender: ${sender}, TokenValue: ${_tokenValue}`);
 
     async function mintTokens() {
         try {
@@ -48,8 +62,20 @@ lockContract.on("LockEvent", async (sender, _tokenValue) => {
     mintTokens()
 });
 
+burnTokensContract.on("tokenBurn", async(_from, _value) => {
+    console.log(`Event Log Detected: From: ${_from}, TokenValue: ${_value}`);
 
-// Token Burn if Token Burn
+    async function addTokens() {
+        try {
+            const addTokens = await ethereumContract.tokenBurnedOnBaseChain(_from, _value);
+            await addTokens.wait();
+            console.log("Tokens Added to User Account: ", addTokens.hash);
+        } catch(error) {
+            console.log(error);
+        }
+    }
+    addTokens();
+});
 
 app.use(express.json());
 
