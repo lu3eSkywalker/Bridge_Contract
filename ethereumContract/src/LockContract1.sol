79f12715f9147069173c35f5d433efbe08b77ce4
lu3eSkywalker <surajjhajhria1@gmail.com>
2024-12-20 19:23:49 +0530

    Adding userTokenBalance & tokenBurnedOnBaseChain function to the ethereum Smart contract

--- ethereumContract/src/LockContract1.sol
+++ ethereumContract/src/LockContract1.sol
@@ -39,7 +39,7 @@ contract LockETH is Ownable {
     function withdraw(address _tokenAddress, uint256 _tokenValue) public {
         require(tokenAddress == _tokenAddress, "The token address should be same as the above");
         uint256 _tokenBalance = balanceOfToken[msg.sender];
-        require(_tokenBalance > _tokenValue, "The Token Doesn't Exist on this Contract");
+        require(_tokenBalance >= _tokenValue, "The Token Doesn't Exist on this Contract");
 
         balanceOfToken[msg.sender] -= _tokenValue;
         IERC20 token = IERC20(_tokenAddress);
@@ -50,15 +50,12 @@ contract LockETH is Ownable {
         emit WithDrawEvent(msg.sender, _tokenValue);
     }
 
-    function unlockTokens(address _to, uint256 _tokenValue) public onlyOwner {
-        IERC20 token = IERC20(tokenAddress);
+    function tokenBurnedOnBaseChain(address _to, uint256 _tokenValue) public onlyOwner {
+        require(_tokenValue > 0, 'The token value should be greater than zero');
+        balanceOfToken[_to] += _tokenValue;
+    }
 
-        uint256 contractBalance = token.balanceOf(address(this));
-        require(contractBalance >= _tokenValue, "Not Sufficient Tokens in the Contract");
-
-        bool success = token.transfer(_to, _tokenValue);
-        require(success, "Error Transferring the tokens");
-
-        emit tokenUnlock(_to, _tokenValue);
+    function userTokenBalance(address _userAddress) public view returns(uint256) {
+        return balanceOfToken[_userAddress];
     }
 }
